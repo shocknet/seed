f4cf40bdcf803f92e8e6c14920b7b5d8b5bbebf0
Erfan Besharat <erbesharat@gmail.com>
2020-06-03 03:21:16 +0430

    (database): Add TypeORM
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,37 @@
+import { ConnectionOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export type Config = {
   port: number;
+  db: ConnectionOptions;
 };
 
 export default {
   port: process.env.PORT || 3000,
+  db: ParseDatabase(),
 } as Config;
+
+// ParseDatabase generates a database connection using environment variables,
+// and based on the database type
+function ParseDatabase() {
+  switch (process.env.DB_TYPE) {
+    case 'postgres':
+      return {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST || 'localhost',
+        port: process.env.POSTGRES_PORT || 5432,
+        username: process.env.POSTGRES_USERNAME || 'shockseed',
+        password: process.env.POSTGRES_PASSWORD || 'shockseed',
+        database: process.env.POSTGRES_DB || 'shockseed',
+        entities: [__dirname + '/models/*.ts'],
+        synchronize: true,
+      };
+    default:
+      return {
+        type: 'sqlite',
+        database: process.env.SQLITE_DB || [__dirname + '/../database.sqlite'],
+      };
+  }
+}
