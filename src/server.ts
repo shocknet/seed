3dad4dc342dae17ce9a33129ac92bdfcbba224bd
Erfan Besharat <erbesharat@gmail.com>
2020-06-03 03:21:16 +0430

    (database): Add TypeORM
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
-import express from "express";
-import registerRoutes from "./routes";
+import express from 'express';
+import { createConnection, Connection } from 'typeorm';
+
+import registerRoutes from './routes';
+import config from './config';
 
 export const app: {
+  db: Connection;
   server: express.Express;
 } = {
+  db: null,
   server: null,
 };
 
 export const bootstrap = async () => {
   app.server = express();
+
+  app.db = await createConnection(config.db);
+
+  await app.db.runMigrations();
 
   registerRoutes(app.server);
 
