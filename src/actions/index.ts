22726afae6e17da9c349e1d7a187c84282e99458
Erfan Besharat <erbesharat@gmail.com>
2020-06-04 03:48:56 +0430

    (actions): Add file upload route and action
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,7 @@
 import enroll from './enroll/enroll';
+import upload from './upload/upload';
 
 export default {
   enroll: enroll,
+  upload: upload,
 };
---
+++ src/models/File.ts
@@ -0,0 +1,64 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Token } from './Token';
+
+@Entity()
+export class File {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+  })
+  originalname: string;
+
+  @Column({
+    nullable: false,
+  })
+  encoding: string;
+
+  @Column({
+    nullable: false,
+  })
+  mimetype: string;
+
+  @Column({
+    nullable: false,
+  })
+  size: number;
+
+  @Column({
+    nullable: false,
+  })
+  destination: string;
+
+  @Column({
+    nullable: false,
+  })
+  filename: string;
+
+  @Column({
+    nullable: false,
+  })
+  path: string;
+
+  @Column({
+    nullable: true,
+  })
+  hash: string;
+
+  @ManyToOne(type => Token, token => token.files)
+  token: Token;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
