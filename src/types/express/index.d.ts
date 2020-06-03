630b3aa1f0b72f2a0eb10d8d59874596e02da477
Erfan Besharat <erbesharat@gmail.com>
2020-06-04 02:22:55 +0430

    (config): Fix storage config and multer interface
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -11,6 +11,7 @@ import * as bodyParser from 'body-parser';
 import serveStatic = require('serve-static');
 import * as core from 'express-serve-static-core';
 import * as qs from 'qs';
+import { Readable } from 'stream';
 import { Token } from '../../models/Token';
 
 /**
@@ -118,6 +119,35 @@ declare namespace e {
   export interface Response<ResBody = any> extends core.Response<ResBody> {}
   interface Router extends core.Router {}
   interface Send extends core.Send {}
+  interface File {
+    /** Name of the form field associated with this file. */
+    fieldname: string;
+    /** Name of the file on the uploader's computer. */
+    originalname: string;
+    /**
+     * Value of the `Content-Transfer-Encoding` header for this file.
+     * @deprecated since July 2015
+     * @see RFC 7578, Section 4.7
+     */
+    encoding: string;
+    /** Value of the `Content-Type` header for this file. */
+    mimetype: string;
+    /** Size of the file in bytes. */
+    size: number;
+    /**
+     * A readable stream of this file. Only available to the `_handleFile`
+     * callback for custom `StorageEngine`s.
+     */
+    stream: Readable;
+    /** `DiskStorage` only: Directory to which this file has been uploaded. */
+    destination: string;
+    /** `DiskStorage` only: Name of this file within `destination`. */
+    filename: string;
+    /** `DiskStorage` only: Full path to the uploaded file. */
+    path: string;
+    /** `MemoryStorage` only: A Buffer containing the entire file. */
+    buffer: Buffer;
+  }
 }
 
 export = e;
