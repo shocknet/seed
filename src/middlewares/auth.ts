27808ccc8e848bc705dd1e1b3298d330707b3fc3
Erfan Besharat <erbesharat@gmail.com>
2020-06-13 20:32:22 +0430

    (actions): Add error handling to upload and auth
    Signed-off-by: Erfan Besharat <erbesharat@gmail.com>

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,19 +3,21 @@ import { app } from '../server';
 import { Token } from '../models/Token';
 
 export default async (req: Request, res: Response, next) => {
-  let TokenRepository = app.db.getRepository(Token);
+  const TokenRepository = app.db.getRepository(Token);
 
-  const token = (await TokenRepository.findOne({
+  const token = await TokenRepository.findOne({
     where: {
       token: req.headers.authorization.slice('Bearer '.length),
     },
-  }).catch(() => {
+  });
+
+  if (!token) {
     return res.status(403).json({
       error: {
         message: "Couldn't find the provided token",
       },
     });
-  })) as Token;
+  }
 
   if (token.used) {
     return res.status(403).json({
